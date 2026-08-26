78d51f2337021bdd53319c361f98f86a79f6b4e8
ikenna <agugbuenzubechi@gmail.com>
2026-08-26 22:41:16 +0100

    updated dashboard icons

--- app/dashboard/reviews/page.tsx
+++ app/dashboard/reviews/page.tsx
@@ -45,12 +45,12 @@ export default function ReviewsPage() {
     return (
       <>
         <div className="dash-welcome">
-          <div className="dash-welcome-eyebrow">
+        <div className="dash-welcome-eyebrow">
           <span className="dash-welcome-icon">
-            <StarHalf size={25} strokeWidth={2.5}  />
-            </span>
-            <span>Reviews</span>
-          </div>
+            <StarHalf size={19} strokeWidth={2.5} />
+          </span>
+          <span>REVIEWS</span>
+        </div>
           <h1>
             What customers are <span>saying</span>.
           </h1>
@@ -69,9 +69,12 @@ export default function ReviewsPage() {
 
   return (
     <>
-      <div className="dash-welcome">
+       <div className="dash-welcome">
         <div className="dash-welcome-eyebrow">
-          <span className="dot"></span>Reviews
+          <span className="dash-welcome-icon">
+            <StarHalf size={19} strokeWidth={2.5} />
+          </span>
+          <span>REVIEWS</span>
         </div>
         <h1>
           What customers are <span>saying</span>.
